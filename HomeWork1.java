6bae0682ad2987d5e0f43741716bf843e602a5b1
A bit Soul™ <116040643+IvanSvyshch@users.noreply.github.com>
2022-10-20 17:32:22 +0200

    Cursor HW#1

---
+++ HomeWork1.java
@@ -0,0 +1,31 @@
+package com.javacursor.lecture1;
+
+public class HomeWork1 {
+
+    public static void main(String[] args) {
+
+        byte a = 5;
+        short b = 15;
+        int e = 25;
+        long c = 10000L;
+        float f = 2.12f;
+        double d = 25.252;
+        char myChar = '\u00A9'; // for example primitive type 'char'
+
+        int plus = a + b;
+        long minus = e - b;
+        double multiplication = d * f;
+        long division = c / e;
+
+        double sumValue = c + a - b * e / f; //for examples sum of all arithmetic operations
+
+        System.out.println("plus " + plus);
+        System.out.println("minus " + minus);
+        System.out.println("multiplication " + multiplication);
+        System.out.println("division " + division);
+        System.out.println("sumValue " + sumValue);
+        System.out.println(myChar);
+
+
+    }
+}
